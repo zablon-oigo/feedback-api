d1972d43300eafc16c863df034c3dd2c8fc08ae3
zablon <zablonoigo51@gmail.com>
2024-12-26 14:30:17 +0300

    Submit new feedback

--- index.js
+++ index.js
@@ -28,3 +28,30 @@ const getFeedback = async (event) => {
 
     return response;
 };
+
+const createFeedback = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const body = JSON.parse(event.body);
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: marshall(body || {}),
+        };
+        const createResult = await db.send(new PutItemCommand(params));
+        response.body = JSON.stringify({
+            message: "Successfully created feedback.",
+            createResult,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to create feedback",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
