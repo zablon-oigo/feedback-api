beab3e899daff906c5fd935aecf6c8f60153bcbe
zablon <zablonoigo51@gmail.com>
2024-12-29 07:48:46 +0300

    format file according to terraform standard

--- feedback/infra/main.tf
+++ feedback/infra/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = var.region
+  region     = var.region
   secret_key = var.secret_key
   access_key = var.access_key
 }
 resource "random_pet" "random" {
-    length = 8
+  length = 8
 }
 resource "aws_s3_bucket" "bucket" {
-    bucket = random_pet.random.id
-    force_destroy = true
+  bucket        = random_pet.random.id
+  force_destroy = true
 }
