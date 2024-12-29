bd50fe5b804f7bbd6363fbcb0061ff8d7f0ad16e
zablon <zablonoigo51@gmail.com>
2024-12-29 22:00:17 +0300

    create cloudfront distribution

--- feedback/infra/main.tf
+++ feedback/infra/main.tf
@@ -41,3 +41,48 @@ EOF
 locals {
   s3_origin_id = "MyS3Origin"
 }
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "feedback-cloudfront"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
