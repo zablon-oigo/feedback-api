8f565aafe62d3d829d15383d1fee5908342f0a14
zablon <zablonoigo51@gmail.com>
2024-12-29 08:34:02 +0300

    add correct source path

--- feedback/infra/main.tf
+++ feedback/infra/main.tf
@@ -4,9 +4,18 @@ provider "aws" {
   access_key = var.access_key
 }
 resource "random_pet" "random" {
-  length = 8
+  length = 6
 }
 resource "aws_s3_bucket" "bucket" {
-  bucket        = random_pet.random.id
+  bucket        = "frontend-${random_pet.random.id}"
   force_destroy = true
 }
+resource "aws_s3_object" "upload_object" {
+  for_each = fileset("${path.module}/../dist", "**/*")
+
+  bucket = aws_s3_bucket.bucket.id
+  key    = each.value
+  source = "${path.module}/../dist/${each.value}"  
+  etag   = filemd5("${path.module}/../dist/${each.value}") 
+}
+
