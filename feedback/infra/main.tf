ce689454ea94e16c7cd9b63240d1482482c6811d
zablon <zablonoigo51@gmail.com>
2024-12-29 21:52:57 +0300

    Add policy to allow access to s3

--- feedback/infra/main.tf
+++ feedback/infra/main.tf
@@ -18,3 +18,23 @@ resource "aws_s3_object" "upload_object" {
   etag     = filemd5("${path.module}/../dist/${each.value}")
 }
 
+resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
